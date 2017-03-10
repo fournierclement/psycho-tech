d551f11da8a6640cce8a7adb9b4813fccac64720
Fournier Clément <fournier.clt@gmail.Com>
2017-03-10 08:18:36 +0100

    Devlping (front) : Footer and Navbar => Translating .php to .jsx to use react (pre-render)

---
+++ components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+/**
+* @desc Footer to place on each web page.
+*/
+export const Footer = ({user}) => (
+  <footer id="Footer" className="ui inverted vertical footer segment">
+    <div className="ui center aligned container">
+      <div className="ui stackable inverted divided grid">
+        <div className="seven wide column">
+          <h4 className="ui inverted header"> Plan du site </h4>
+          <div className="ui inverted link list">
+            <a href="" className="item"> Accueil </a>
+            { user || <a href="" className="item"> S\'enregistrer </a> }
+            { user && user.admin && <a href="" className="item"> Statistiques </a> }
+            { user && user.admin && <a href="" className="item"> Dashboard </a> }
+            { user && user.results && <a href="" className="item"> Questionnaire </a> }
+            { user && <a href="" className="item"> Mon Profil </a> }
+            { user ? <a href="" className="item"> Déconnexion </a> : <a href="" className="item"> Connexion </a> }
+          </div>
+        </div>
+        <div className="nine wide column">
+          <h4 className="ui inverted header"> Création du site </h4>
+          <p> Ce site a été réalisé dans le cadre d'un projet scolaire. </p>
+          <p>
+            Basé sur du pur MVC PHP et mise en forme par
+            <a href="http://semantic-ui.com/" title="Framework CSS" target="_blank">
+              Semantic-UI
+            </a>
+          </p>
+          <p>
+            Voir le dépôt officiel :
+            <a href="https://github.com/kevinhassan/PiscineProjectIG3" target="_blank">
+              <i className="github icon"></i>
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="ui inverted section divider">
+      </div>
+      <img src="favicon.ico" className="ui centered mini image">
+      <div className="ui horizontal inverted small divided link list">
+        <a className="item" href="#"> Plan du site </a>
+        <a className="item" href="#"> Nous contacter </a>
+        <a className="item" href="#"> Termes & Conditions </a>
+        <a className="item" href="#"> Politique de vie privée </a>
+        <div className="ui inverted section">
+          <p> Kévin Hassan <i className="copyright icon"></i> 2017 </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+)
---
+++ components/Navbar.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+export const Navbar = ({user, page}) => (
+  <div id="Navbar">
+    <nav className="ui left vertical inverted labeled icon thin sidebar menu">
+      <a className={ page === "Accueil" ? "active item" : "item" } href="">
+        <i className="home icon"></i>
+        Accueil
+      </a>
+      { user && ( !user.admin ? (
+        <a className={ page === "Questionnaire" ? "active item" : "item" } href="">
+          <i class="info icon"></i>
+          Questionnaire
+        </a>
+      ) : (
+        <a className={ page === "Statistiques" ? "active item" : "item" } href="">
+          <i class="bar chart icon"></i>
+          Statistiques
+        </a>
+      ))}
+      { user && user.admin && (
+        <a className={ page === "Dashboard" ? "active item" : "item" } href="">
+          <i class="dashboard icon"></i>
+          Dashboard
+        </a>
+      )}
+      { user || (
+        <a className={ page === "S\'enregistrer" ? "active item" : "item" } href="">
+          <i class="save icon"></i>
+          S'enregistrer
+        </a>
+      )}
+      { user && (
+        <a className={ page === "Mon Profile" ? "active item" : "item" } href="">
+          <i class="user icon"></i>
+          Mon Profile
+        </a>
+      )}
+      { user && user.admin && (
+        <a href="https://kevinhassan.github.io/riasec/" title="documentation" class="item"><i class="book icon"></i> Documentation </a>
+      )}
+      { user ? (
+        <a className={ page === "Connexion" ? "active item" : "item" } href="">
+          <i class="sign in icon"></i>
+          Connexion
+        </a>
+      ) : (
+        <a className={ page === "Déconnexion" ? "active item" : "item" } href="">
+          <i class="sign out icon"></i>
+          Déconnexion
+        </a>
+      )}
+    </nav>
+    <nav class="ui fixed inverted menu">
+      <div class="ui container">
+        <div class="ui compact icon large secondary inverted pointing menu">
+        <a class="toc item">
+          <i class="sidebar icon"></i>
+          <span class="text"> Menu </span>
+        </a>
+          { user && ( !user.admin ? (
+            <a className={ page === "Questionnaire" ? "active item" : "item" } href="">
+              <i class="info icon"></i>
+              Questionnaire
+            </a>
+          ) : (
+            <a className={ page === "Statistiques" ? "active item" : "item" } href="">
+              <i class="bar chart icon"></i>
+              Statistiques
+            </a>
+          ))}
+        </div>
+        <div class="right menu">
+          { user && user.admin && (
+            <a className={ page === "Dashboard" ? "active item" : "item" } href="">
+              <i class="dashboard icon"></i>
+              Dashboard
+            </a>
+          )}
+          { user || (
+            <a className={ page === "S\'enregistrer" ? "active item" : "item" } href="">
+              <i class="save icon"></i>
+              S'enregistrer
+            </a>
+          )}
+          { user && (
+            <a className={ page === "Mon Profile" ? "active item" : "item" } href="">
+              <i class="user icon"></i>
+              Mon Profile
+            </a>
+          )}
+          { user && user.admin && (
+            <a href="https://kevinhassan.github.io/riasec/" title="documentation" class="item"><i class="book icon"></i> Documentation </a>
+          )}
+          { user ? (
+            <a className={ page === "Connexion" ? "active item" : "item" } href="">
+              <i class="sign in icon"></i>
+              Connexion
+            </a>
+          ) : (
+            <a className={ page === "Déconnexion" ? "active item" : "item" } href="">
+              <i class="sign out icon"></i>
+              Déconnexion
+            </a>
+          )}
+        </div>
+      </div>
+    </nav>
+  </div>
+)
